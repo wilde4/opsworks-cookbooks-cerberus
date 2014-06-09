f8b146c94bc9407d9a3bebc95c8c1861aca69b87
Jamie van Dyke <jamie@fearoffish.com>
2014-06-09 14:06:43 +0100

    debugging

--- theme_sync/recipes/default.rb
+++ theme_sync/recipes/default.rb
@@ -1,5 +1,5 @@
 nodes = search(:node, "name:*")
-nodes.each {|n| Chef::Log.info "====> #{n["layers"]}"}
+nodes.each {|n| Chef::Log.info "====> #{n.layers}"}
 app_servers = nodes.select {|n| n["layers"]["rails-app"] rescue nil }
 
 template '/home/deploy/theme_sync.sh' do
