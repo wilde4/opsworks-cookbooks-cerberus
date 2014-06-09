6699a8c5d2ab1c8000cfb2719769c7511991c31f
Jamie van Dyke <jamie@fearoffish.com>
2014-06-09 12:54:23 +0100

    debugging

--- theme_sync/recipes/default.rb
+++ theme_sync/recipes/default.rb
@@ -1,5 +1,8 @@
+Chef::Logger.info "Node info: #{search(:node, "name:*")}"
+
 template '/home/deploy/theme_sync.sh' do
   source "theme_sync.sh.erb"
+  owner "deploy"
   mode "0777"
   variables(:opsworks => node[:opsworks])
 end
