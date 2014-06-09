54cdf52ec1ae9d7e4920d6625af81e1784164d88
Jamie van Dyke <jamie@fearoffish.com>
2014-06-09 13:27:49 +0100

    debugging

--- theme_sync/recipes/default.rb
+++ theme_sync/recipes/default.rb
@@ -1,8 +1,11 @@
 Chef::Log.info "Node info: #{search(:node, "name:*")}"
 
+nodes = search(:node, "name:*")
+app_servers = nodes.select {|n| n["layers"]["rails-app"]
+
 template '/home/deploy/theme_sync.sh' do
   source "theme_sync.sh.erb"
   owner "deploy"
   mode "0777"
-  variables(:nodes => search(:node, "name:*"))
+  variables(:nodes => app_servers)
 end
