54e3651327dc7e521141c1f4de562959a803d27e
Robert Wilde <r.wilde@volcanic.co.uk>
2014-07-03 12:49:02 +0100

    no need to change working directory

--- pull_themes/recipes/default.rb
+++ pull_themes/recipes/default.rb
@@ -1,5 +1,4 @@
 execute "rsync themes and assets" do
-  cwd deploy[:current_path]
   command "rsync -avz -e ssh deploy@#{deploy[:pull_themes][:ip]}:/cloud9/sync /srv/www/oliver/shared"
   user "deploy"
 end
