86f66dc66164cd2c9ac348822113d3eaafa3a391
Robert Wilde <r.wilde@volcanic.co.uk>
2014-07-03 14:31:44 +0100

    requirement on ssh_keys for pull_themes

--- pull_themes/recipes/default.rb
+++ pull_themes/recipes/default.rb
@@ -1,3 +1,5 @@
+include_recipe "ssh_keys"
+
 execute "rsync themes and assets" do
   command "rsync -avz -e ssh deploy@#{node[:pull_themes][:ip]}:/cloud9/sync /srv/www/oliver/shared"
   user "deploy"
