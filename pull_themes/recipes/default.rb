c83e01469e97b524ddbfc535823be26eaefb0612
Robert Wilde <r.wilde@volcanic.co.uk>
2014-07-03 12:55:35 +0100

    node reference

--- pull_themes/recipes/default.rb
+++ pull_themes/recipes/default.rb
@@ -1,4 +1,4 @@
 execute "rsync themes and assets" do
-  command "rsync -avz -e ssh deploy@#{deploy[:pull_themes][:ip]}:/cloud9/sync /srv/www/oliver/shared"
+  command "rsync -avz -e ssh deploy@#{node[:pull_themes][:ip]}:/cloud9/sync /srv/www/oliver/shared"
   user "deploy"
 end
