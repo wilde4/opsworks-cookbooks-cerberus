48e81d872e72820f3de90fe6eaa6b191e4deda52
Robert Wilde <r.wilde@volcanic.co.uk>
2014-07-03 15:01:47 +0100

    updated theme_sync cookbook

--- theme_sync/recipes/default.rb
+++ theme_sync/recipes/default.rb
@@ -1,9 +1,40 @@
-nodes = search(:node, "name:*")
-app_servers = nodes.select {|n| n["opsworks"]["layers"]["rails-app"] rescue nil }
-
-template '/home/deploy/theme_sync.sh' do
-  source "theme_sync.sh.erb"
+# ======================================
+# = Allow Devbox to push theme changes =
+# ======================================
+template '/home/deploy/.ssh/authorized_keys' do
+  source "authorized_keys.erb"
   owner "deploy"
-  mode "0777"
-  variables(:nodes => app_servers)
+  mode "0600"
 end
+
+# ==============================================
+# = Allow App Server to pull themes are deploy =
+# ==============================================
+template '/home/deploy/.ssh/id_rsa' do
+  source "id_rsa.erb"
+  owner "deploy"
+  mode "0600"
+end
+
+# ===================
+# = Pull Themes Now =
+# ===================
+execute "rsync themes and assets" do
+  command "rsync -avz -e ssh deploy@#{node[:theme_sync][:ip]}:/cloud9/sync /srv/www/oliver/shared"
+  user "deploy"
+end
+
+
+# ==========================================================
+# = Update Push script so theme deploys to all App Servers =
+# ==========================================================
+
+# nodes = search(:node, "name:*")
+# app_servers = nodes.select {|n| n["opsworks"]["layers"]["rails-app"] rescue nil }
+#
+# template '/home/deploy/theme_sync.sh' do
+#   source "theme_sync.sh.erb"
+#   owner "deploy"
+#   mode "0777"
+#   variables(:nodes => app_servers)
+# end
