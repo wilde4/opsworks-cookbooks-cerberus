3c51aff27429d0897d7e4c804c614682257209f8
Ben Gibbs <ben@bobop.co.uk>
2015-08-03 12:22:54 +0100

    edited rsync_setup_themes

--- theme_sync/recipes/rsync_setup_themes.rb
+++ theme_sync/recipes/rsync_setup_themes.rb
@@ -3,7 +3,7 @@
 # ===================
 execute "trigger rsync of themes and assets" do
   # CURL COMMAND WITH #{node[:opsworks][:instance][:public_dns_name]}
-  command 'curl -s -X POST -H "Content-Type: application/json" -d "{ "public_dns_name": "#{node[:opsworks][:instance][:public_dns_name]" }" http://deploy.volcanic.co.uk/api/v1/themes/setup_rsync.json'
+  command "curl -s -X POST -H 'Content-Type: application/json' -d '{ \"public_dns_name\": \"#{node[:opsworks][:instance][:public_dns_name]\" }' http://deploy.volcanic.co.uk/api/v1/themes/setup_rsync.json"
   # STACK NAME FOR LIMITING THEMES TO RSYNC: node[:opsworks][:stack][:name]
   # command "rsync -arvce 'ssh -o StrictHostKeyChecking=no' --delete /srv/www/precompile_app/shared/sync deploy@#{node[:opsworks][:instance][:public_dns_name]}:/srv/www/oliver/shared"
   user "deploy"
