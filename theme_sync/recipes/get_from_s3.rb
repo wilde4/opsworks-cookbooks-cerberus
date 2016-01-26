1d20f25e67c63c73c8019ed0cd3cfd9933a3feb6
Ben Gibbs <ben@bobop.co.uk>
2016-01-26 16:00:23 +0000

    added config file path to s3 command

--- theme_sync/recipes/get_from_s3.rb
+++ theme_sync/recipes/get_from_s3.rb
@@ -1,6 +1,6 @@
 # DOWNLOAD ALL THEMES AND ASSETS FROM S3 BUCKET
 
 execute "download themes and assets from s3" do
-  command "s3cmd get --recursive --config=/home/deploy/.s3cfg s3://oliver-themes /srv/www/oliver/shared/sync/"
+  command "s3cmd get --recursive --config=/home/deploy/.s3cfg s3://oliver-themes /srv/www/oliver/shared/sync"
   user "deploy"
 end
