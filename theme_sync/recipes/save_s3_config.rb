a4d4931b221745e658d40d42e931b1e481b9a3bc
Ben Gibbs <ben@bobop.co.uk>
2016-01-26 15:24:54 +0000

    added root s3 config file

--- theme_sync/recipes/save_s3_config.rb
+++ theme_sync/recipes/save_s3_config.rb
@@ -1,15 +1,13 @@
 # SAVE S3 CONFIG FILE
 
-package "python"
-package "python-setuptools"
-package "python-distutils-extra"
-package "python-dateutil"
-package "python-requests"
-
-package "s3cmd"
-
 template '/home/deploy/.s3cfg' do
   source "s3cfg.erb"
   owner "deploy"
+  mode "0600"
+end
+
+template '/root/.s3cfg' do
+  source "s3cfg.erb"
+  owner "root"
   mode "0600"
 end
