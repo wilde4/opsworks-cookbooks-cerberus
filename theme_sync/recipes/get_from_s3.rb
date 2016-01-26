3c2625d3279cd3756371b5deaecc227a3dfb7863
Ben Gibbs <ben@bobop.co.uk>
2016-01-26 17:23:43 +0000

    added --continue to s3cmd get

--- theme_sync/recipes/get_from_s3.rb
+++ theme_sync/recipes/get_from_s3.rb
@@ -1,7 +1,7 @@
 # DOWNLOAD ALL THEMES AND ASSETS FROM S3 BUCKET
 
 execute "download themes and assets from s3" do
-  command "s3cmd --recursive --config=/home/deploy/.s3cfg get s3://oliver-themes /srv/www/oliver/shared/sync"
+  command "s3cmd --recursive --continue --config=/home/deploy/.s3cfg get s3://oliver-themes /srv/www/oliver/shared/sync"
   # command "s3cmd get --recursive --config=/home/deploy/.s3cfg s3://oliver-themes /srv/www/oliver/shared/sync"
   user "deploy"
 end
