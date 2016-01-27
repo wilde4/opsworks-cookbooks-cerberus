8a900b18b43e505e69084f45e9f1f06b920c142b
Ben Gibbs <ben@bobop.co.uk>
2016-01-27 19:57:18 +0000

    added new theme rsync from sibling

---
+++ theme_sync/recipes/rsync_themes_from_sibling.rb
@@ -0,0 +1,12 @@
+# RSYNC THEMES FROM SIBLING
+
+# nodes = search(:node, "name:*")
+# theme_servers = nodes.select {|n| n["opsworks"]["layers"]["themes"] rescue nil }
+
+theme_server = search(:node, "layers:themes").first
+Chef::Log.info("**********The private IP address is: '#{theme_server[:private_ip]}'**********")
+
+execute "rsync themes and assets" do
+  command "rsync -arvce 'ssh -o StrictHostKeyChecking=no' --delete deploy@#{theme_server[:private_ip]}:/srv/www/oliver/shared/sync /srv/www/oliver/shared"
+  user "deploy"
+end
