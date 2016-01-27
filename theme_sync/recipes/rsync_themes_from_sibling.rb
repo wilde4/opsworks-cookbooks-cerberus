b9ec3eb6aa5262dfab97fa76aa40263a44481395
Ben Gibbs <ben@bobop.co.uk>
2016-01-27 20:51:32 +0000

    added new theme rsync from sibling

--- theme_sync/recipes/rsync_themes_from_sibling.rb
+++ theme_sync/recipes/rsync_themes_from_sibling.rb
@@ -3,7 +3,7 @@
 # nodes = search(:node, "name:*")
 # theme_servers = nodes.select {|n| n["opsworks"]["layers"]["themes"] rescue nil }
 
-theme_server = search(:node, "layers:themes").first
+theme_server = search(:node, "layers:themes").last
 Chef::Log.info("**********The private IP address is: '#{theme_server[:private_ip]}'**********")
 
 execute "rsync themes and assets" do
