dd618ce974a028742add2f9f107f9bdd6955235c
Ben Gibbs <ben@bobop.co.uk>
2016-02-02 11:35:11 +0000

    adapting rsync_setup_themes

--- theme_sync/recipes/rsync_setup_themes.rb
+++ theme_sync/recipes/rsync_setup_themes.rb
@@ -2,8 +2,10 @@
 # = Trigger Volcanic Deploy RSYNC =
 # ===================
 execute "trigger rsync of themes and assets" do
-  # CURL COMMAND WITH #{node[:opsworks][:instance][:public_dns_name]}
-  command "curl -s -X POST -H 'Content-Type: application/json' -d '{ \"public_dns_name\": \"#{node[:opsworks][:instance][:public_dns_name]}\" }' http://deploy.volcanic.co.uk/api/v1/themes/setup_rsync.json"
+  # CURL COMMAND WITH #{node[:opsworks][:instance][:private_ip]}
+  Chef::Log.info("********** The private IP address for RSYNC is: '#{node[:opsworks][:instance][:private_ip]}' **********")
+  # command "curl -s -X POST -H 'Content-Type: application/json' -d '{ \"private_ip\": \"#{node[:opsworks][:instance][:private_ip]}\" }' http://deploy.volcanic.co.uk/api/v1/themes/setup_rsync.json"
+  
   # STACK NAME FOR LIMITING THEMES TO RSYNC: node[:opsworks][:stack][:name]
   # command "rsync -arvce 'ssh -o StrictHostKeyChecking=no' --delete /srv/www/precompile_app/shared/sync deploy@#{node[:opsworks][:instance][:public_dns_name]}:/srv/www/oliver/shared"
   user "deploy"
