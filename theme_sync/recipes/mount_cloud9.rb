382f64678a5a0036c1bb72a8d7384c5c82ceaa0e
Ben Gibbs <ben@bobop.co.uk>
2016-02-23 17:02:29 +0000

    added remote_development to mount_cloud9 path

--- theme_sync/recipes/mount_cloud9.rb
+++ theme_sync/recipes/mount_cloud9.rb
@@ -1,4 +1,4 @@
-directory "/srv/www/oliver/shared/themes" do
+directory "/srv/www/oliver/shared/themes/remote_development" do
   mode 0755
   owner 'deploy'
   group 'www-data'
@@ -7,6 +7,6 @@ directory "/srv/www/oliver/shared/themes" do
 end
 
 execute "mount cloud9 to themes" do
-  command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/oliver/shared/themes"
+  command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/oliver/shared/themes/remote_development"
   user "root"
 end
