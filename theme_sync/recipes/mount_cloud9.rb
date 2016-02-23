f89f273787b5c1fe088225a81e81576effc87d95
Ben Gibbs <ben@bobop.co.uk>
2016-02-23 14:49:07 +0000

    added theme directory creation to mount_cloud9

--- theme_sync/recipes/mount_cloud9.rb
+++ theme_sync/recipes/mount_cloud9.rb
@@ -1,3 +1,11 @@
+directory "/srv/www/oliver/shared/themes" do
+  mode 0755
+  owner 'deploy'
+  group 'www-data'
+  recursive true
+  action :create
+end
+
 execute "mount cloud9 to themes" do
   command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/oliver/shared/themes"
   user "root"
