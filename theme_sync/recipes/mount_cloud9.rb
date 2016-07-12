b79b4dad8f848c985b976396e5a839b90591df13
Ben Gibbs <ben@bobop.co.uk>
2016-07-12 13:15:21 -0700

    make sure NFS mount does not try if already mounted

--- theme_sync/recipes/mount_cloud9.rb
+++ theme_sync/recipes/mount_cloud9.rb
@@ -9,5 +9,5 @@ end
 execute "mount cloud9 to themes" do
   command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/oliver/shared/themes/remote_development"
   user "root"
-  not_if do ::File.exists?('/srv/www/oliver/shared/remote_development/production/eventbeat/layout/theme.liquid') end
+  not_if do ::File.exists?('/srv/www/oliver/shared/themes/remote_development/production/eventbeat/layout/theme.liquid') end
 end
