c6676b6e17a63a7eecf87b99219516613622f06c
Ben Gibbs <ben@bobop.co.uk>
2016-07-13 14:10:47 -0700

    added CHef Log to see why auth key is not found

--- theme_sync/recipes/authorized_keys_setup.rb
+++ theme_sync/recipes/authorized_keys_setup.rb
@@ -2,6 +2,7 @@
 # = Create Authorized Keys =
 # ======================================
 template '/home/deploy/.ssh/authorized_keys' do
+  Chef::Log.info("********** /home/deploy/.ssh/authorized_keys. node[:theme_sync][:authorized_keys]: '#{node[:theme_sync][:authorized_keys]}' **********")
   source "authorized_keys.erb"
   owner "deploy"
   mode "0600"
