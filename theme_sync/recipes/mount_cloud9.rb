010c4041e0107321ade695528d316443536b8c2e
Ben Gibbs <ben@bobop.co.uk>
2016-07-13 11:56:51 -0700

    corrected paths in mount_cloud9.rb

--- theme_sync/recipes/mount_cloud9.rb
+++ theme_sync/recipes/mount_cloud9.rb
@@ -1,4 +1,4 @@
-directory "/srv/www/oliver/shared/themes/remote_development" do
+directory "/srv/www/oliver/shared/themes" do
   mode 0755
   owner 'deploy'
   group 'www-data'
@@ -7,7 +7,7 @@ directory "/srv/www/oliver/shared/themes/remote_development" do
 end
 
 execute "mount cloud9 to themes" do
-  command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/oliver/shared/themes/remote_development"
+  command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/oliver/shared/themes"
   user "root"
-  not_if do ::File.exists?('/srv/www/oliver/shared/themes/remote_development/eventbeat/layout/theme.liquid') end
+  not_if do ::File.exists?('/srv/www/oliver/shared/themes/eventbeat/layout/theme.liquid') end
 end
