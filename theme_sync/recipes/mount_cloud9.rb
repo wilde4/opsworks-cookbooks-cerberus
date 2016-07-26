9e7fcf14fd2bdfba26a28646b555edddf5549f46
Ben Gibbs <ben@bobop.co.uk>
2016-07-26 16:09:02 -0700

    new mount routine for Themes

--- theme_sync/recipes/mount_cloud9.rb
+++ theme_sync/recipes/mount_cloud9.rb
@@ -1,4 +1,12 @@
-directory "/srv/www/oliver/shared/themes" do
+# directory "/srv/www/oliver/shared/themes" do
+#   mode 0755
+#   owner 'deploy'
+#   group 'www-data'
+#   recursive true
+#   action :create
+# end
+
+directory "/srv/www/themes" do
   mode 0755
   owner 'deploy'
   group 'www-data'
@@ -7,7 +15,12 @@ directory "/srv/www/oliver/shared/themes" do
 end
 
 execute "mount cloud9 to themes" do
-  command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/oliver/shared/themes"
+  command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/themes"
   user "root"
   not_if do ::File.exists?('/srv/www/oliver/shared/themes/eventbeat/layout/theme.liquid') end
+end
+
+execute "create symlink to themes" do
+  command "ln -s /srv/www/themes/ /srv/www/oliver/shared/themes"
+  user "deploy"
 end
