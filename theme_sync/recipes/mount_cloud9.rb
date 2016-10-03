2a74372b95b67e8a204de711134bfc14d2d11831
Ben Gibbs <ben@bobop.co.uk>
2016-10-03 16:56:12 +0100

    cahnged the mount IP address

--- theme_sync/recipes/mount_cloud9.rb
+++ theme_sync/recipes/mount_cloud9.rb
@@ -15,7 +15,7 @@ directory "/srv/www/themes" do
 end
 
 execute "mount cloud9 to themes" do
-  command "sudo mount 10.0.1.200:/cloud9/cloud9 /srv/www/themes"
+  command "sudo mount 10.0.0.42:/cloud9/cloud9 /srv/www/themes"
   user "root"
   not_if do ::File.exists?('/srv/www/oliver/shared/themes/eventbeat/layout/theme.liquid') end
 end
