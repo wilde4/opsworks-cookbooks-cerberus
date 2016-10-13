e30c323174b8b1bea529811eeb9bd512ce621ba0
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2016-10-13 10:16:35 +0100

    add log lines to setup_nfs

--- theme_sync/recipes/setup_nfs.rb
+++ theme_sync/recipes/setup_nfs.rb
@@ -1,10 +1,12 @@
 template '/etc/exports' do
+  Chef::Log.info("********** Writing /etc/exports **********")
   source "exports.erb"
   owner "root"
   mode "0600"
 end
 
 execute "start nfs service" do
+  Chef::Log.info("********** Starting NFS service **********")
   command "service nfs-kernel-server start"
   user "root"
 end
