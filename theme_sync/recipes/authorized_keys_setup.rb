366157cec89b65244d156f30f6b0fb7a76d9f571
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2016-10-13 10:41:04 +0100

    create ssh folder if doesnt exist

--- theme_sync/recipes/authorized_keys_setup.rb
+++ theme_sync/recipes/authorized_keys_setup.rb
@@ -1,6 +1,12 @@
 # ======================================
 # = Create Authorized Keys =
 # ======================================
+execute "create deploy/authorized_keys file" do
+  command "mkdir /home/deploy/.ssh"
+  user "deploy"
+  not_if do ::File.exists?('/home/deploy/.ssh/authorized_keys') end
+end
+
 template '/home/deploy/.ssh/authorized_keys' do
   Chef::Log.info("********** node: '#{node.inspect}' **********")
   Chef::Log.info("********** node[:theme_sync]: '#{node[:theme_sync].inspect}' **********")
