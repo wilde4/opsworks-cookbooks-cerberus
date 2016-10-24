9ade7e1e30bffca5e22e140bc11f0d344efab3b6
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2016-10-24 11:03:57 +0100

    make rsync setup themes more generic

--- theme_sync/recipes/rsync_setup_themes.rb
+++ theme_sync/recipes/rsync_setup_themes.rb
@@ -4,7 +4,7 @@
 execute "trigger rsync of themes and assets" do
   # CURL COMMAND WITH #{node[:opsworks][:instance][:private_ip]}
   Chef::Log.info("********** The private IP address for RSYNC is: '#{node[:opsworks][:instance][:private_ip]}' **********")
-  command "curl -s -X POST -H 'Content-Type: application/json' -d '{ \"private_ip\": \"#{node[:opsworks][:instance][:private_ip]}\" }' http://deploy.volcanic.co.uk/api/v1/themes/setup_rsync.json"
+  command "curl -s -X POST -H 'Content-Type: application/json' -d '{ \"private_ip\": \"#{node[:opsworks][:instance][:private_ip]}\" }' #{node[:theme_sync][:theme_rsync_endpoint]}"
   user "deploy"
-  not_if do ::File.exists?('/srv/www/oliver/shared/sync/themes/production/eventbeat/layout/theme.liquid') end
+  not_if do ::File.exists?("/srv/www/oliver/shared/sync/themes/production/#{node[:theme_sync][:theme_to_test_for]}/layout/theme.liquid") end
 end
