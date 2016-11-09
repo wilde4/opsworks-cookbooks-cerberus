8ead7ca6fd31ac479ed3ff49d6eee513d8cc51f3
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2016-11-09 13:28:44 +0000

    create c9 folder on nfs

--- theme_sync/recipes/setup_nfs.rb
+++ theme_sync/recipes/setup_nfs.rb
@@ -5,6 +5,12 @@ template '/etc/exports' do
   mode "0600"
 end
 
+execute "create cloud9 folder" do
+  Chef::Log.info("********** Creating Clou9 folder **********")
+  command "mkdir /cloud9/cloud9"
+  user "root"
+end
+
 execute "start nfs service" do
   Chef::Log.info("********** Starting NFS service **********")
   command "service nfs-kernel-server start"
