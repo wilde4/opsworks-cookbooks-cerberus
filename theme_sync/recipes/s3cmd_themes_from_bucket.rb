2422c0f69f86453d1cb2f52d8b701ab7f6efb717
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-02-01 16:01:36 +0000

    do both themes and assets as separate commands

--- theme_sync/recipes/s3cmd_themes_from_bucket.rb
+++ theme_sync/recipes/s3cmd_themes_from_bucket.rb
@@ -1,8 +1,14 @@
 # ===================
 # = Trigger Theme Sync from S3 =
 # ===================
-execute "trigger s3cmd sync of themes and assets" do
-  command "s3cmd sync --recursive --config=/home/deploy/.s3cfg s3://#{node[:theme_sync][:theme_bucket_name]} /srv/www/oliver/shared/sync/"
+execute "trigger s3cmd sync ofassets" do
+  command "s3cmd sync --recursive --config=/home/deploy/.s3cfg s3://#{node[:theme_sync][:theme_bucket_name]}/precompiled_assets/ /srv/www/oliver/shared/sync/precompiled_assets/"
   user "deploy"
-  not_if do ::File.exists?("/srv/www/oliver/shared/sync/themes/production/#{node[:theme_sync][:theme_to_test_for]}/layout/theme.liquid") end
+  # not_if do ::File.exists?("/srv/www/oliver/shared/sync/themes/production/#{node[:theme_sync][:theme_to_test_for]}/layout/theme.liquid") end
+end
+
+execute "trigger s3cmd sync of themes" do
+  command "s3cmd sync --recursive --config=/home/deploy/.s3cfg s3://#{node[:theme_sync][:theme_bucket_name]}/themes/ /srv/www/oliver/shared/sync/themes/"
+  user "deploy"
+  # not_if do ::File.exists?("/srv/www/oliver/shared/sync/themes/production/#{node[:theme_sync][:theme_to_test_for]}/layout/theme.liquid") end
 end
