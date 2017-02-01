744373930df2a051e93e40336ae31315a71bfe80
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-02-01 14:23:02 +0000

    specify s3cmd config location

--- theme_sync/recipes/s3cmd_themes_from_bucket.rb
+++ theme_sync/recipes/s3cmd_themes_from_bucket.rb
@@ -2,7 +2,7 @@
 # = Trigger Theme Sync from S3 =
 # ===================
 execute "trigger s3cmd sync of themes and assets" do
-  command "s3cmd sync --recursive s3://#{node[:theme_sync][:theme_bucket_name]} /srv/www/oliver/shared/sync/"
+  command "s3cmd sync --recursive --config=/home/deploy/.s3cfg s3://#{node[:theme_sync][:theme_bucket_name]} /srv/www/oliver/shared/sync/"
   user "deploy"
   not_if do ::File.exists?("/srv/www/oliver/shared/sync/themes/production/#{node[:theme_sync][:theme_to_test_for]}/layout/theme.liquid") end
 end
