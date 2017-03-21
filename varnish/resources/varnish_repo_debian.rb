dba4b0f82f89626a5afc7ed7175cc526cd106c33
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-03-21 14:33:08 +0000

    try simple package

--- varnish/resources/varnish_repo_debian.rb
+++ varnish/resources/varnish_repo_debian.rb
@@ -2,11 +2,17 @@ provides :varnish_repo, platform_family: 'debian'
 
 # default_action :configure
 
-apt_repository 'varnish-cache_4.1' do
-  uri "http://repo.varnish-cache.org/#{node['platform']}"
-  distribution node['lsb']['codename']
-  components ['varnish-4.1']
-  key "https://repo.varnish-cache.org/#{node['platform']}/GPG-key.txt"
-  deb_src true
-  action :add
+# apt_repository 'varnish-cache_4.1' do
+#   uri "http://repo.varnish-cache.org/#{node['platform']}"
+#   distribution node['lsb']['codename']
+#   components ['varnish-4.1']
+#   key "https://repo.varnish-cache.org/#{node['platform']}/GPG-key.txt"
+#   deb_src true
+#   action :add
+# end
+
+package 'varnish' do
+  # package_name 'varnish-4.1'
+  version '4.1'
+  action :install
 end
