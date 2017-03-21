3a44d461d71f18506b5f424188008225a0ab34d9
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-03-21 13:45:45 +0000

    hardcode major version

--- varnish/resources/varnish_repo_debian.rb
+++ varnish/resources/varnish_repo_debian.rb
@@ -2,8 +2,7 @@ provides :varnish_repo, platform_family: 'debian'
 
 # default_action :configure
 
-
-apt_repository "varnish-cache_#{new_resource.major_version}" do
+apt_repository 'varnish-cache_4.1' do
   uri "http://repo.varnish-cache.org/#{node['platform']}"
   distribution node['lsb']['codename']
   components ['varnish-4.1']
@@ -11,4 +10,3 @@ apt_repository "varnish-cache_#{new_resource.major_version}" do
   deb_src true
   action :add
 end
-
