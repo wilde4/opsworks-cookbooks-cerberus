75843d815643e7cdf030283cf010b4b7e9d41f1d
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-03-21 13:28:01 +0000

    more cahnges

--- varnish/resources/varnish_repo_debian.rb
+++ varnish/resources/varnish_repo_debian.rb
@@ -1,14 +1,14 @@
 provides :varnish_repo, platform_family: 'debian'
 
-default_action :configure
+# default_action :configure
 
-action :configure do
-  apt_repository "varnish-cache_#{new_resource.major_version}" do
-    uri "http://repo.varnish-cache.org/#{node['platform']}"
-    distribution node['lsb']['codename']
-    components ['varnish-4.1']
-    key "https://repo.varnish-cache.org/#{node['platform']}/GPG-key.txt"
-    deb_src true
-    action :add
-  end
+
+apt_repository "varnish-cache_#{new_resource.major_version}" do
+  uri "http://repo.varnish-cache.org/#{node['platform']}"
+  distribution node['lsb']['codename']
+  components ['varnish-4.1']
+  key "https://repo.varnish-cache.org/#{node['platform']}/GPG-key.txt"
+  deb_src true
+  action :add
 end
+
