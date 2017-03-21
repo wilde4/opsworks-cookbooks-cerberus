ad0cdd061be060ce8b56085d056d1371973b5bd7
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-03-21 15:15:10 +0000

    setup varnish apt repository

--- varnish/recipes/install_varnish.rb
+++ varnish/recipes/install_varnish.rb
@@ -1,5 +1,14 @@
-include_recipe 'apt'
+execute "add_varnish_repo_key" do
+  command "curl https://repo.varnish-cache.org/GPG-key.txt | sudo apt-key add -"
+  user "root"
+end
 
+execute "add_varnish_repo" do
+  command 'echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.1" | sudo tee -a /etc/apt/sources.list.d/'
+  user "root"
+end
+
+include_recipe 'apt'
 
 package 'varnish' do
   # package_name 'varnish-4.1'
