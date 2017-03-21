1afcb69fc7a99836bd14725a1a9b991e2a5a56d6
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-03-21 15:21:43 +0000

    fix typo

--- varnish/recipes/install_varnish.rb
+++ varnish/recipes/install_varnish.rb
@@ -4,7 +4,7 @@ execute "add_varnish_repo_key" do
 end
 
 execute "add_varnish_repo" do
-  command 'echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.1" | sudo tee -a /etc/apt/sources.list.d/'
+  command 'echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.1" | sudo tee -a /etc/apt/sources.list.d/varnish-cache.list'
   user "root"
 end
 
