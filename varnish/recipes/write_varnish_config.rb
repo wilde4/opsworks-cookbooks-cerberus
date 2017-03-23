5aab81330c14cf1101de2150038f64d93c7fb5d5
Matthew Whiteley <m.whiteley@volcanic.co.uk>
2017-03-23 11:51:30 +0000

    varnish config templates

--- varnish/recipes/write_varnish_config.rb
+++ varnish/recipes/write_varnish_config.rb
@@ -5,3 +5,19 @@ template "/etc/varnish/backends.vcl" do
   source "backends.vcl.erb"
   # variables(:deploy => deploy, :application => application)
 end
+
+template "/etc/varnish/custom.vcl" do
+  mode '0755'
+  owner deploy[:user]
+  group deploy[:group]
+  source "custom.vcl.erb"
+  # variables(:deploy => deploy, :application => application)
+end
+
+template "/etc/varnish/default.vcl" do
+  mode '0755'
+  owner deploy[:user]
+  group deploy[:group]
+  source "default.vcl.erb"
+  # variables(:deploy => deploy, :application => application)
+end
---
+++ varnish/recipes/write_varnish_daemon_config.rb
@@ -0,0 +1,11 @@
+template "/etc/default/varnish" do
+  mode '0755'
+  owner root
+  # group deploy[:group]
+  source "varnish.erb"
+  # variables(:deploy => deploy, :application => application)
+end
+
+service "varnish" do
+  action :restart
+end
